128c5bbd418a04c560023aa004be67b63182cbf5
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2023-05-27 23:53:28 +0600

    Write dockerfile

---
+++ db/init_db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS mydb;
+
+CREATE DATABASE mydb;
+
+USE mydb;
+
+DROP TABLE IF EXISTS mytable;
+
+CREATE TABLE mytable (
+	id INT NOT NULL AUTO_INCREMENT,
+	data LONGTEXT NOT NUL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO mytable (data) VALUES("initial data");
