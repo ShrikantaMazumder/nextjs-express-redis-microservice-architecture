729a60325e32b211ee2a3bb8dc43063ca176b355
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2023-05-30 09:38:19 +0600

    fix docker files and add worker server

--- db/init_db.sql
+++ db/init_db.sql
@@ -6,10 +6,11 @@ USE mydb;
 
 DROP TABLE IF EXISTS mytable;
 
-CREATE TABLE mytable (
-	id INT NOT NULL AUTO_INCREMENT,
-	data LONGTEXT NOT NUL,
-	PRIMARY KEY (id)
+CREATE TABLE mytable ( 
+  id INT NOT NULL AUTO_INCREMENT, 
+  data LONGTEXT NOT NULL, 
+  PRIMARY KEY (id) 
 );
 
-INSERT INTO mytable (data) VALUES("initial data");
+
+INSERT INTO mytable (data) VALUES('initial data');
